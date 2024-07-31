e74f878e21f9cc5b47c77088b22d25df3f8d8e67
Alex <lxndmrr@gmail.com>
2024-07-31 20:52:18 +0300

    change user id type to Long

--- src/main/java/com/github/qalx/lurker/model/User.java
+++ src/main/java/com/github/qalx/lurker/model/User.java
@@ -10,15 +10,15 @@ import jakarta.persistence.Table;
 public class User {
     @Id
     @GeneratedValue
-    long id;
+    Long id;
     String name;
     String password;
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
