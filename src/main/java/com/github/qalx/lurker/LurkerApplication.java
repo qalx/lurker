c331d5401f502d4e7135ad6471960d5362afa312
Alex <lxndmrr@gmail.com>
2024-07-31 19:31:47 +0300

    initialize spring boot project

---
+++ src/main/java/com/github/qalx/lurker/LurkerApplication.java
@@ -0,0 +1,13 @@
+package com.github.qalx.lurker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LurkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LurkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/qalx/lurker/LurkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.qalx.lurker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LurkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
